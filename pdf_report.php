b6faeab40c972a974b0451fa4c8699d32a88c846
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-02-09 20:24:38 +0300

    Update pdf_report.php

--- pdf_report.php
+++ pdf_report.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -16,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Report for competency.
+ * PDF report for competency analysis.
  *
  * @package    local_yetkinlik
  * @copyright  2026 Hakan Çiğci {@link https://hakancigci.com.tr}
@@ -67,7 +66,7 @@ require_once(__DIR__ . '/ai.php');
 $comment = local_yetkinlik_generate_comment($rates);
 $course = $DB->get_record('course', ['id' => $courseid]);
 
-/* PDF İşlemleri */
+/* PDF İşlemleri. */
 $pdf = new TCPDF();
 $pdf->AddPage();
 $pdf->SetFont('freeserif', '', 12);
