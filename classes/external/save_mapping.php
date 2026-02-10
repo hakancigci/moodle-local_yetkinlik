0510df42e2fa8c20a5dc24df338fd0402261d177
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-02-10 18:10:14 +0300

    Update save_mapping.php

--- classes/external/save_mapping.php
+++ classes/external/save_mapping.php
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
  * External API class for competency mapping operations.
@@ -38,7 +38,6 @@ use context_course;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class save_mapping extends external_api {
-
     /**
      * Describes the parameters for execute.
      *
