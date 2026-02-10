e5fd1f4068dc25f1109afe4eefe5d140a40adc97
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-02-10 18:12:03 +0300

    Update provider.php

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
  * Local plugin "yetkinlik" - Privacy provider.
@@ -34,7 +34,6 @@ use core_privacy\local\metadata\null_provider;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements null_provider {
-
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
