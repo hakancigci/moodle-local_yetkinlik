4d39c77089fbc215eecf8fe604b85462779ea28e
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-02-19 03:07:56 +0300

    Update local_yetkinlik.php

--- lang/en/local_yetkinlik.php
+++ lang/en/local_yetkinlik.php
@@ -133,3 +133,6 @@ $string['timeline'] = 'Timeline';
 $string['timelineheading'] = 'Competency Progress Over Time';
 $string['total'] = 'TOTAL';
 $string['user'] = 'Student';
+$string['yetkinlik:manage'] = 'Manage question-competency mappings';
+$string['yetkinlik:viewreports'] = 'View all student competency reports';
+$string['yetkinlik:viewownreport'] = 'View own competency analysis report';
