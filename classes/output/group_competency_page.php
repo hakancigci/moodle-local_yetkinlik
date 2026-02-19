6290ddd17227f8d44106fd5a7fba17c71a9db5a6
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-02-19 19:48:10 +0300

    Update group_competency_page.php

--- classes/output/group_competency_page.php
+++ classes/output/group_competency_page.php
@@ -21,7 +21,7 @@
  * @copyright  2026 Hakan Çiğci {@link https://hakancigci.com.tr}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
- 
+
 namespace local_yetkinlik\output;
 
 use renderable;
@@ -29,28 +29,54 @@ use templatable;
 use renderer_base;
 use stdClass;
 
+/**
+ * Renderable class for the group competency report page.
+ *
+ * @package    local_yetkinlik
+ * @copyright  2026 Hakan Çiğci {@link https://hakancigci.com.tr}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class group_competency_page implements renderable, templatable {
+
+    /** @var int The course ID. */
     protected $courseid;
+
+    /** @var int The group ID. */
     protected $groupid;
+
+    /** @var stdClass The data to be rendered. */
     protected $data;
 
+    /**
+     * Constructor.
+     *
+     * @param int $courseid
+     * @param int $groupid
+     * @param stdClass $data
+     */
     public function __construct($courseid, $groupid, $data) {
         $this->courseid = $courseid;
         $this->groupid = $groupid;
         $this->data = $data;
     }
 
+    /**
+     * Export data for the Mustache template.
+     *
+     * @param renderer_base $output
+     * @return stdClass
+     */
     public function export_for_template(renderer_base $output) {
         $export = new stdClass();
         $export->courseid = $this->courseid;
         $export->has_group = $this->groupid > 0;
-        
-        // Use array_values to ensure sequential keys (0, 1, 2...) for Mustache loops.
+
+        // Ensure sequential keys (0, 1, 2...) for Mustache loops.
         $export->groups = !empty($this->data->groups) ? array_values((array)$this->data->groups) : [];
         $export->competencies = !empty($this->data->competencies) ? array_values((array)$this->data->competencies) : [];
         $export->students = !empty($this->data->students) ? array_values((array)$this->data->students) : [];
         $export->totals = !empty($this->data->totals) ? array_values((array)$this->data->totals) : [];
-        
+
         return $export;
     }
 }
