b831b5d0834045a6dff352ed0aa3500c987cb181
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-02-20 05:34:21 +0300

    Update group_quiz_competency_page.php

--- classes/output/group_quiz_competency_page.php
+++ classes/output/group_quiz_competency_page.php
@@ -37,7 +37,6 @@ use stdClass;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class group_quiz_competency_page implements renderable, templatable {
-
     /** @var int The course ID. */
     protected $courseid;
 
