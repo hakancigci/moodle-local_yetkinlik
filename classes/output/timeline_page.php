a3d012cf0957770c94f1ffc3560e9391d6ae2cd0
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-02-20 05:39:33 +0300

    Update timeline_page.php

--- classes/output/timeline_page.php
+++ classes/output/timeline_page.php
@@ -38,7 +38,6 @@ use stdClass;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class timeline_page implements renderable, templatable {
-
     /** @var stdClass Data object containing timeline periods and competency datasets. */
     protected $data;
 
