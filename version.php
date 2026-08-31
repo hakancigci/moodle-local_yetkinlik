cae85770bf056a4342948203d626152d65b5c182
Hakan ÇİĞCİ <47192002+hakancigci@users.noreply.github.com>
2026-08-31 13:26:32 +0300

    Update version.php

--- version.php
+++ version.php
@@ -26,10 +26,10 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'local_yetkinlik';       // Full name of the plugin (category_name).
-$plugin->version   = 2026083001;              // The current module version (YYYYMMDDXX).
+$plugin->version   = 2026083101;              // The current module version (YYYYMMDDXX).
 $plugin->requires  = 2024042210;              // Requires Moodle 4.5 or later.
 $plugin->maturity  = MATURITY_STABLE;          // Stable release.
-$plugin->release   = '3.0.10';                 // Human-readable version name.
+$plugin->release   = '3.0.11';                 // Human-readable version name.
 
 // Plugin dependencies (Other plugins that must be installed first).
 $plugin->dependencies = [
